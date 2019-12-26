b2f48bc37d1eab16971b894d53ca05f56003481f
i11007020 <i11007020@gmail.com>
2019-12-26 19:34:46 +0800

    commit

--- src/main/java/ZipkinServer/ZipkinServerApplication.java
+++ src/main/java/ZipkinServer/ZipkinServerApplication.java
@@ -4,9 +4,11 @@ package ZipkinServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
